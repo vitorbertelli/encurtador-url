bba033de7c0b874f5b1c2d5e2dd2bc231b3a6a37
Vitor Bertelli <vitorbertelli19@gmail.com>
2023-07-28 08:53:48 -0300

    fixing

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -4,22 +4,23 @@ const urlShortener = async (req, res) => {
   try {
     const originalUrl = req.body;
     const objectUrl = await Url.create(originalUrl);
-    const shortUrl = "http://localhost:3000/" + objectUrl.hash;
-    console.log(objectUrl, shortUrl)
-    res.status(201).json(shortUrl);
+    const shortUrl = "http://localhost:8000/" + objectUrl.hash;
+    res.status(201).json({
+      url: shortUrl
+    });
   } catch(err) {
     res.status(400).json(err.message);
   }
-}
+} 
 
 const redirectUrl = async (req, res) => {
   try {
     const { hash } = req.params;
     const objectUrl = await Url.findOne({ "hash": hash });
-    console.log(objectUrl)
     res.status(302).redirect(objectUrl.originalUrl);
   } catch(err) {
     res.status(400).json(err.message);
   }
 }
+
 export { urlShortener, redirectUrl };
